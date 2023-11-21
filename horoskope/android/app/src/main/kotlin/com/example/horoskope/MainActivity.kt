e1348e5fd4c5d014ff09f1bc4f9c94729c7bb8ee
dimagamera <46674070+dimagamera@users.noreply.github.com>
2023-11-21 17:41:58 +0200

    Delete horoskope directory

--- horoskope/android/app/src/main/kotlin/com/example/horoskope/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.horoskope
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- horoskope/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:horoskope/screen_menu.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
